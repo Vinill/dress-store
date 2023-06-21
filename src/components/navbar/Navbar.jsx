c4f10c39a3b4857df04e51e1be437a266fbfb305
Daniel Muller <98336206+Vinill@users.noreply.github.com>
2023-06-21 12:56:19 -0300

    more changes

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className='nav-o'>
+      Navbar
+    </div>
+  );
+};
+
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navbar } from "../../components/navbar/Navbar";
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <div className='back'>
+
+    </div>
+  );
+};
