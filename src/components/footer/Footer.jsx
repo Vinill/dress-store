6d858efe3aabac95c347c93ec95b5fc79de29ff4
Daniel Muller <98336206+Vinill@users.noreply.github.com>
2023-07-03 11:57:33 -0300

    empezamos con el footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import "./Footer.css"
+import InstagramIcon from '@mui/icons-material/Instagram';
+import IconButton from '@mui/material/IconButton';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 
 export const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='footer'>
+      <div>
+        <IconButton  sx={{color:"black"}}>
+          <InstagramIcon style={{fontSize:"35px"}}/>
+        </IconButton>
+        <IconButton  sx={{color:"black"}}>
+          <FacebookIcon style={{fontSize:"35px"}}/>
+        </IconButton>
+        <IconButton  sx={{color:"black"}}>
+          <TwitterIcon style={{fontSize:"35px"}}/>
+        </IconButton>
+      </div>
+      <p>info - Suport - Marketing</p>
+      <p>Term of Use - Privacity Policy</p>
+      <p>2023 Daniel Muller</p>
     </div>
   );
 };
--- src/components/nav-center/NavCenter.jsx
+++ src/components/nav-center/NavCenter.jsx
@@ -1,49 +1,25 @@
 import React from 'react'
 import "./NavCenter.css";
-import { IoIosMan } from "react-icons/io";
-import { IoIosWoman } from "react-icons/io";
-import { FaChildren } from "react-icons/fa6";
 import Button from '@mui/material/Button';
+import ManIcon from '@mui/icons-material/Man';
+import WomanIcon from '@mui/icons-material/Woman';
+import BoyIcon from '@mui/icons-material/Boy';
 
 
 export const NavCenter = () => {
 
   return (
     <div className='nav'>
-      <Button className='buttonY' variant="text" sx={{
-        color:'black',
-        borderColor:"black",
-        height:"70px",
-        width:"80px",
-        textAlign:"center",
-        display:"flex",
-        flexDirection:"column",
-        }}>
-        <IoIosMan style={{fontSize:"30px"}} />
+      <Button className='buttonY' sx={{color:'black',}}>
+        <ManIcon style={{fontSize:"30px"}}/>
         Hombre
       </Button>
-      <Button className='buttonY' sx={{
-        color:'black',
-        borderColor:"black",
-        height:"70px",
-        width:"80px",
-        textAlign:"center",
-        display:"flex",
-        flexDirection:"column",
-        }}>
-        <IoIosWoman style={{fontSize:"30px"}} />
+      <Button className='buttonY' sx={{color:'black',}}>
+        <WomanIcon style={{fontSize:"30px"}}/>
         Mujeres
       </Button>
-      <Button className='buttonY' sx={{
-        color:'black',
-        borderColor:"black",
-        height:"70px",
-        width:"80px",
-        textAlign:"center",
-        display:"flex",
-        flexDirection:"column",
-        }}>
-        <FaChildren style={{fontSize:"30px"}} />
+      <Button className='buttonY' sx={{color:'black',}}>
+        <BoyIcon style={{fontSize:"30px"}}/>
         Niños
       </Button>
     </div>
