d59af6db21f5c105b6a973d9a5d6059874ffaca8
Daniel Muller <98336206+Vinill@users.noreply.github.com>
2023-07-17 13:36:21 -0700

    nuevos cambios

--- src/components/body-home/BodyHome.jsx
+++ src/components/body-home/BodyHome.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import "./BodyHome.css" 
-import imgBanner from "./../../imgs/hermosa-modelo-morena-femenina-ropa-blanca_149155-260-removebg-preview.png"
+import imgBanner from "./../../imgs/882666651_1-removebg-preview.png"
+import imgOne from "./../../imgs/free-photo-of-mujer-fotografia-de-moda-fondo-blanco-tejanos-azules-removebg-preview.png"
 
 export const BodyHome = () => {
   return (
     <div className='body-home'>
       <div className='banner'>
         <img src={imgBanner} alt="img" className='banner-img'/>
+      </div>
+      <div className='section'>
+        <div className='one'>
+          <img src={imgOne} alt="img" className='imgs-down'/>
+        </div>
+        <div className='one'>
+          <img src={imgOne} alt="img" className='imgs-down'/>
+        </div>
+        <div className='one'>
+          <img src={imgOne} alt="img" className='imgs-down'/>
+        </div>
       </div>
     </div>
   );
