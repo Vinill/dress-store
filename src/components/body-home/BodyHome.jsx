041c4cefcbd27677e8b75a47144cd68530fe137c
Daniel Muller <98336206+Vinill@users.noreply.github.com>
2023-07-17 12:42:00 -0700

    push more changes

--- src/components/body-home/BodyHome.jsx
+++ src/components/body-home/BodyHome.jsx
@@ -1,23 +1,13 @@
 import React from 'react'
-import "./BodyHome.css"
-import logo from "./../../imgs/LOGO_OPC3-removebg-preview.png"
-import imgBanner from "./../../imgs/310781821_619671119794473_3259522790867787573_n 1.png"
+import "./BodyHome.css" 
+import imgBanner from "./../../imgs/hermosa-modelo-morena-femenina-ropa-blanca_149155-260-removebg-preview.png"
 
 export const BodyHome = () => {
   return (
     <div className='body-home'>
       <div className='banner'>
-        <div className='banner-img'>
-          <img src={imgBanner} alt="img" className='banner-img_img'/>
-        </div>
-        <div className='banner-logo'>
-          <img src={logo} alt='img' className='banner-logo_img'/>
-        </div>
+        <img src={imgBanner} alt="img" className='banner-img'/>
       </div>
-      <div className='comp-one'><img src="" alt="" srcset="" /></div>
-      <div className='comp-two'><img src="" alt="" srcset="" /></div>
-      <div className='comp-three'><img src="" alt="" srcset="" /></div>
-      <div className='comp-four'><img src="" alt="" srcset="" /></div>
     </div>
   );
 };
--- src/components/nav-center/NavCenter.jsx
+++ src/components/nav-center/NavCenter.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
 import "./NavCenter.css";
 import Button from '@mui/material/Button';
-import ManIcon from '@mui/icons-material/Man';
-import WomanIcon from '@mui/icons-material/Woman';
-import BoyIcon from '@mui/icons-material/Boy';
-
 
 export const NavCenter = () => {
 
   return (
     <div className='nav'>
-      <Button className='buttonY' sx={{color:'black',}}>
-        <ManIcon style={{fontSize:"30px"}}/>
-        Hombre
+      <Button className='buttonY' sx={{color:'black', fontSize:'12px'}}>
+        INICIO
       </Button>
-      <Button className='buttonY' sx={{color:'black',}}>
-        <WomanIcon style={{fontSize:"30px"}}/>
-        Mujeres
+      <Button className='buttonY' sx={{color:'black', fontSize:'12px'}}>
+        TIENDA
       </Button>
-      <Button className='buttonY' sx={{color:'black',}}>
-        <BoyIcon style={{fontSize:"30px"}}/>
-        Niños
+      <Button className='buttonY' sx={{color:'black', fontSize:'12px'}}>
+        OFERTAS
+      </Button>
+      <Button className='buttonY' sx={{color:'black', fontSize:'12px'}}>
+        PAGINAS
+      </Button>
+      <Button className='buttonY' sx={{color:'black', fontSize:'12px'}}>
+        CONTACTO
       </Button>
     </div>
   );
